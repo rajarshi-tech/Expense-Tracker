bf44e28ee47ba0a4e36197ed80896063e8244292
Rajarshi Roy <rajarshiroy2005456@gmail.com>
2026-02-08 20:58:11 +0530

    added login route

--- expense-tracker/src/App.jsx
+++ expense-tracker/src/App.jsx
@@ -4,6 +4,7 @@ import { Charts } from './pages/Charts'
 
 import './App.css'
 import { ExpenseProvider } from './context/ExpenseContext'
+import { Login } from './pages/Login'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route index element={<HomePage />} />
         <Route path="charts" element={<Charts />} />
+        <Route path="sign-in" element={<Login />} />
       </Routes>
     </ExpenseProvider>
   )
--- expense-tracker/src/pages/Login.jsx
+++ expense-tracker/src/pages/Login.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import "./Login.css";
 
-function Login() {
+export function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [showPassword, setShowPassword] = useState(false);
@@ -57,4 +57,3 @@ function Login() {
   );
 }
 
-export default Login;
