261c492eed9477a4ce990ed07af193dae127c803
Rajarshi Roy <rajarshiroy2005456@gmail.com>
2026-02-08 20:32:39 +0530

    added firebase.js

---
+++ expense-tracker/src/firebase.js
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCbys3_1GIwtUHqzR10ZHTPI8SAsx6-RZ8",
+  authDomain: "expense-tracker-f610f.firebaseapp.com",
+  projectId: "expense-tracker-f610f",
+  storageBucket: "expense-tracker-f610f.firebasestorage.app",
+  messagingSenderId: "568428613685",
+  appId: "1:568428613685:web:fff5993248961faf1e617c",
+  measurementId: "G-S0SDP0L54S"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const analytics = getAnalytics(app);
+export const firestore = getFirestore(app);
+
